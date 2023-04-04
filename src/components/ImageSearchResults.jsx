4b3c17d49f21746e793d7133ea5dc4d41ecf5990
Carlos flores <scarlo1@hotmail.com>
2023-04-04 13:33:41 -0500

    add pagination component

--- src/components/ImageSearchResults.jsx
+++ src/components/ImageSearchResults.jsx
@@ -1,9 +1,10 @@
 import Link from 'next/link'
 import React from 'react'
+import PaginationButtons from './PaginationButtons'
 
 export default function ImageSearchResults({ results }) {
   return (
-    <div className="pb-24 mt-4">
+    <div className="sm:pb-24 pb-40 mt-4">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-3 space-x-4">
             {results.items.map((result) => (
                 <div key={result.link} className="group">
@@ -18,6 +19,9 @@ export default function ImageSearchResults({ results }) {
                     </Link>
                 </div>
             ))}
+        </div>
+        <div className="ml-16">
+            <PaginationButtons />
         </div>
     </div>
   )
